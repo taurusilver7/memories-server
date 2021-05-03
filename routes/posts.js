396d0a8bca21b45e957dde9712051234d26624bb
taurusilver7 <prasad20697@gmail.com>
2021-05-03 20:11:36 +0530

    sample get route created

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,10 @@
 import express from "express";
 
 const router = express.Router();
+
+// http://localhost:500/posts/
+router.get("/", (req, res) => {
+  res.send("This works");
+});
+
+export default router;
