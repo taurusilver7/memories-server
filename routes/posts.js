58268f577dec38e7fd09a854a3136861bcfa980d
taurusilver7 <prasad20697@gmail.com>
2021-05-03 20:19:57 +0530

    route handler added to route

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,9 @@
 import express from "express";
+import { getPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
 // http://localhost:500/posts/
-router.get("/", (req, res) => {
-  res.send("This works");
-});
+router.get("/", getPosts);
 
 export default router;
