adf831a4ac14f8138b918f42f501ed557425f4b3
taurusilver7 <prasad20697@gmail.com>
2021-05-03 20:28:38 +0530

    get & post handle fns created

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,24 @@
 // the route handler functions for the server routes.
+import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-  res.send("This works!");
+export const getPosts = async (req, res) => {
+  try {
+    const postMsg = await PostMessage.find();
+    console.log(postMsg);
+    res.status(200).json(postMsg);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
